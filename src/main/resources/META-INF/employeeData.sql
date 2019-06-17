42ecb43ee46ec848a6aa524fa9ae56ab882b64cb
Theint Yoon Thinzar <yttheint@aavn.local>
2019-06-17 14:37:44 +0700

    add new employee

--- src/main/resources/META-INF/employeeData.sql
+++ src/main/resources/META-INF/employeeData.sql
@@ -1,3 +1,4 @@
 insert into employee(name,age,email) values ('Yoon',20,'yoon@gmail.com');
 insert into employee(name,age,email) values ('thin',20,'thin@gmail.com');
 insert into employee(name,age,email) values ('sindhu',20,'sindhu@gmail.com');
+insert into employee(name,age,email) values ('thanh',20,'thanh@gmail.com');
