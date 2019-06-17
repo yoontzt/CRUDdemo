8b46fc573bfded0a1f65efeec71b1d20f9505717
Theint Yoon Thinzar <yttheint@aavn.local>
2019-06-17 13:53:42 +0700

    Step 1: Database test only

---
+++ target/classes/META-INF/employeeData.sql
@@ -0,0 +1,3 @@
+insert into employee(name,age,email) values ('Yoon',20,'yoon@gmail.com');
+insert into employee(name,age,email) values ('thin',20,'thin@gmail.com');
+insert into employee(name,age,email) values ('sindhu',20,'sindhu@gmail.com');
