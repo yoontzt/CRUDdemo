a92e5d6ef71e7697120c237ea35b43655c4b536d
Theint Yoon Thinzar <yttheint@aavn.local>
2019-06-18 10:37:09 +0700

    change data.sql

--- src/main/resources/META-INF/employeeData.sql
+++ src/main/resources/META-INF/employeeData.sql
@@ -5,5 +5,5 @@ insert into department(id,name,location) values (4,'HR','Yangon');
 
 insert into employee(name,age,email,department) values ('Yoon',20,'yoon@gmail.com',1);
 insert into employee(name,age,email,department) values ('thin',20,'thin@gmail.com',2);
-insert into employee(name,age,email,department) values ('sindhu',20,'sindhu@gmail.com',1);
-insert into employee(name,age,email,department) values ('thanh',20,'thanh@gmail.com',2);
+insert into employee(name,age,email,department) values ('sindhu',20,'sindhu@gmail.com',3);
+insert into employee(name,age,email,department) values ('thanh',20,'thanh@gmail.com',4);
